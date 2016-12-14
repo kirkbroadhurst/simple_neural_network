8ac02796bb3eb8169668f6b70b01890db825aac3
Kirk <kirk@broadhur.st>
2016-12-13 20:23:12 -0600

    back to develop

--- simple_machine.py
+++ simple_machine.py
@@ -1,46 +1,57 @@
 import numpy as np
 
 
-class SimpleMachine():
+class SimpleMachine(object):
     
-    def __init__(self, training_data, result):
+    def __init__(self, training_data, result, layers):
+        """
+        Create a simple neural network machine for classification
+        :param training_data: input data as numpy matrix, n rows of observations with m features
+        :param result: observed results, n rows of a single value
+        :param layers: an array where each value indicates the size of a hidden layer
+        """
         self.training_data = training_data        
         self.result = result
-        self.synapse = []
-        _, self.features = training_data.shape
-        
+        self.layers = layers
+        self.z = []
+        self.synapse = self.__construct_synapses()
 
     @property
     def size(self):
         return len(self.synapse)
-    
-    
+
     def nonlin(self, x, deriv=False):
         # sigmoid function
         if(deriv):
             return x*(1-x)
-        return 1/(1+np.exp(-x))            
-    
-        
-    def __construct_synapse(self):
-        # add one layer to the net for each feature        
-        j = self.features
-        while j > 1:
-            # add a random mean-centered synapse of size n,n-1 
-            self.synapse += [2*np.random.random((j,j-1))-1]
-            j = j - 1
-            
-    
-    def __forward_propagate(self, data):
-        ######
-        # propogate through layers, applying synapse
-        ######        
-        layers = [data]
-        for i in range(self.size):            
+        return 1/(1+np.exp(-x))
+
+    def __construct_synapses(self):
+        """
+        Constructs a set of randomly initiated synapses corresponding to the number / size of hidden layers
+        :return: Array of matrixes for the synapses
+        """
+        s = []
+        for l in range(len(self.layers)):
+            # for the first layer, the input size is the number of features in training data
+            n = self.training_data.shape[1] if l == 0 else self.layers[l-1]
+            m = self.layers[l]
+
+            # add a random mean-centered synapse of size; 2 * random(0,1) - 1
+            s += [2*np.random.random((n+1, m))-1]
+        return s
+
+    def __forward_propagate(self):
+        """
+        propagate through layers, applying synapses
+        """
+        self.z = []
+        data = [self.training_data]
+        for l in self.layers:
+
             layers += [self.nonlin(layers[i].dot(self.synapse[i]))]
         return layers
-                
-    
+
     def score(self, data):
         return self.__forward_propagate(data)[-1]
     
@@ -73,3 +84,9 @@ class SimpleMachine():
                 print errors[-1]
 
         print layers[-1]
+
+if __name__ == "__main__":
+    t = np.matrix(((1, 1, 1, 1), (2, 2, 2, 2), (3, 3, 3, 3)))
+    l = [3, 2]
+    r = np.matrix(((1), (2), (3)))
+    s = SimpleMachine(t, r, l)
---
+++ tests/simple_tests.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_construct_synapse():
+    assert 1 == 1
