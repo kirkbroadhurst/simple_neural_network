trying to fix backpropagation

lib/network.py
@@ -32,18 +32,34 @@ def forward_propagate(initial_layer, thetas):
     Push input data through the neural network
     :param initial_layer: Initial layer of data (raw features)
     :param thetas: Set of thetas to act in synapses
-    :return: Constructed neural network of
+    :return: Array of input matrices and output matrices
     """
 
+    #TODO remove this a stuff
     data = [initial_layer]
+    #a_ = []
 
     # for each synapse, push through in_data and get out_data
     for i in range(len(thetas)):
         x = data[i]
         theta = thetas[i]
-        out_data = g(z(x, theta))
-        data.append(np.matrix(out_data))
+        #a_.append(a(x))
+        output = g(z(x, theta))
+        data.append(np.matrix(output))
     return data
+
+
+def a(x):
+    """
+    Compute the a value for x with additional bias input
+    :param x: the input value for a matrix computation
+    :return: X with appended column of 1s for bias
+    """
+    # add extra column for bias/constant term
+    a_ = np.matrix(np.empty((x.shape[0], x.shape[1] + 1)))
+    a_[:, -1] = 1
+    a_[:, :-1] = x
+    return a_
 
 
 def z(x, theta):
@@ -53,11 +69,7 @@ def z(x, theta):
     :param theta: The theta value for this synapse
     :return: x*theta including the bias term
     """
-    # add extra column for bias/constant term
-    in_data = np.empty((x.shape[0], x.shape[1] + 1))
-    in_data[:, -1] = 1
-    in_data[:, :-1] = x
-    return in_data.dot(theta)
+    return a(x).dot(theta)
 
 
 def g(x):
@@ -96,8 +108,12 @@ def delta(theta, output_error, activation):
     :param activation: The activation for this layer
     :return: delta for the hidden layer
     """
-    slope = g_prime(z(activation, theta))
-    return theta.dot(output_error.T)*slope
+
+    # construct the activation matrix with an addition bias term
+    a_ = a(activation)
+    slope = np.multiply(a_, 1 - a_)
+    result = np.multiply(theta.dot(output_error.T), slope.T)
+    return result
 
 
 def cost(output, expected):
@@ -110,7 +126,8 @@ def cost(output, expected):
     gap = error(output, expected)
     n = expected.shape[0]
     p = np.multiply(gap, gap)
-    return 1.0/(2*n)*sum(sum(p).T)[0]
+    c = 1.0/(2*n)*sum(sum(p).T)
+    return c[0, 0]
 
 
 

simple_machine.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lib.network import build_synapses, forward_propagate, delta, cost
+from lib.network import build_synapses, forward_propagate, delta, cost, a
 
 
 class SimpleMachine(object):
@@ -15,10 +15,19 @@ class SimpleMachine(object):
         self.Y = result
         self.layers = layers
         self.theta = build_synapses(layers)
+        self.l = 0.01
 
     @property
     def size(self):
         return len(self.theta)
+
+    @property
+    def m(self):
+        """
+        Number of rows in the training data
+        :return:
+        """
+        return self.training_data.shape[0]
 
     def score(self, data):
         """
@@ -26,7 +35,7 @@ class SimpleMachine(object):
         :param data:
         :return:
         """
-        return forward_propagate(data, self.theta)
+        return forward_propagate(data, self.theta)[-1]
 
     def train(self, iterations=10000):
         """
@@ -38,32 +47,38 @@ class SimpleMachine(object):
         np.random.seed(1)
 
         for i in range(iterations):
-            layers = forward_propagate(self.training_data, self.theta)
+            test = i % self.m
+            x = self.training_data[test]
+            y = self.Y[test]
 
-            ######
+            g = forward_propagate(x, self.theta)
             # backward propagate deltas
-            ######
-            
+
             # we need an error matrix for each layer
-            errors = [None] * len(layers)
+            errors = [None] * len(g)
 
             # set the last error value to the actual values minus the last layer
-            errors[-1] = self.Y - layers[-1]
+            errors[-1] = y - g[-1]
 
             # reverse iterate through layers, adjusting synapse according to error
-            for t in range(len(self.theta) - 1, -1, -1):
-                error = delta(self.theta[t], errors[t+1], layers[t])
-                self.theta[t] -= error
-                errors[t] = error
+            for th in range(len(self.theta) - 1, -1, -1):
+                error = delta(self.theta[th], errors[th+1], g[th])
+                self.theta[th] -= error
+                errors[th] = error
 
-            print 'cost', cost(layers[-1], self.Y)
+            big_delta = sum([(a(g[e])*errors[e])[0, 0] for e in range(len(errors) - 1)])
+            print big_delta
 
-        print layers[-1]
+            d0 = 1.0 / self.m * big_delta
+            d = [np.matrix(1.0 / self.m * (big_delta + (self.l * th))) for th in self.theta]
+            self.theta = [th + d[ix] for ix, th in enumerate(self.theta)]
+            print 'big delta', big_delta
+            print 'cost', cost(self.score(self.training_data), self.Y)
 
 
 if __name__ == "__main__":
-    t = np.matrix(((1, 1, 1, 1), (2, 2, 2, 2), (3, 3, 3, 3)))
-    l = [4, 2]
-    r = np.matrix(((1, 0), (0, 1), (1, 0)))
+    t = np.matrix(((1.0, 0, 0, 0.99), (0, 0.8, 0, 0.95), (0, 0, 0.9, 0.9)))
+    l = [4, 3]
+    r = np.matrix(((1, 0, 0), (0, 1, 0), (0, 0, 1)))
     s = SimpleMachine(t, r, l)
-    s.train(5)
+    s.train(100)

test/forward_propagate_test.py
@@ -17,9 +17,10 @@ def test_single_synapse():
 
     assert shape == (4, 4)
 
+    print output[1]
     # values are all between 0.997527 and 0.997528
-    assert (np.zeros(shape) < (output[1] - 0.997527)).all()
-    assert (np.zeros(shape) > (output[1] - 0.997528)).all()
+    assert (np.zeros(shape) < (output[1] - 0.9990889)).all()
+    assert (np.zeros(shape) > (output[1] - 0.9990890)).all()
 
 
 def test_three_synapse():