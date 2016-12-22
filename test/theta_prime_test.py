102f1bf44d0627e41e154d6cbcb3e01aa070c1e5
Kirk <kirk@broadhur.st>
2016-12-21 23:38:28 -0600

    unit test for theta prime method

--- test/theta_prime_test.py
+++ test/theta_prime_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 from lib.network import theta_prime
 from data import load_csv
 
@@ -13,9 +14,14 @@ def test_theta_prime():
     theta1 = load_csv('data/theta1.csv')
     theta2 = load_csv('data/theta2.csv')
 
+    theta1_grad = load_csv('data/theta1_grad.csv')
+    theta2_grad = load_csv('data/theta2_grad.csv')
+
     y = load_csv('data/y.csv')
 
     result = theta_prime([a1, a2, a3], [z2, z3], [theta1, theta2], y)
+    assert np.allclose(result[0], theta1_grad)
+    assert np.allclose(result[1], theta2_grad)
 
 
 if __name__ == "__main__":
