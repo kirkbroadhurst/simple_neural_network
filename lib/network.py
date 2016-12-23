fixing backpropagation; testing cost reduction

lib/network.py
@@ -37,7 +37,7 @@ def forward_propagate(a1, thetas):
          z_n = a_n * theta_n
     """
 
-    a_ = [a1]
+    a_ = [a(a1)]
     z_ = []
 
     # for each synapse, push through in_data and get out_data
@@ -46,7 +46,9 @@ def forward_propagate(a1, thetas):
         theta = thetas[i]
         z_.append(z(x, theta))
         output = g(z_[-1])
-        a_.append(np.matrix(output))
+        a_.append(a(output))
+    # the last a value shouldn't have bias term - it's the network output. Truncate the bias column
+    a_[-1] = a_[-1][:, 1:]
     return a_, z_
 
 
@@ -58,19 +60,19 @@ def a(x):
     """
     # add extra column for bias/constant term
     a_ = np.matrix(np.empty((x.shape[0], x.shape[1] + 1)))
-    a_[:, -1] = 1
-    a_[:, :-1] = x
+    a_[:, 0] = 1
+    a_[:, 1:] = x
     return a_
 
 
-def z(x, theta):
+def z(a_, theta):
     """
-    Compute the z value of x*theta; append the bias term
-    :param x: The input data to this synapse
+    Compute the z value of a*theta; does not append the bias term
+    :param a_: The input data to this synapse
     :param theta: The theta value for this synapse
-    :return: x*theta including the bias term
+    :return: a*theta
     """
-    return a(x).dot(theta)
+    return a_.dot(theta)
 
 
 def g(x):
@@ -151,24 +153,33 @@ def theta_prime(a, z, theta, y):
 
     m = y.shape[0]
 
-    # hard coding to the number of layers, for simplicity
-    a1 = a[0]
-    a2 = a[1]
-    a3 = a[2]
+    # the last a value is the network output
+    out = a[-1]
 
-    theta1 = theta[0]
-    theta2 = theta[1]
+    # compute d values
+    # the first computed d value is the network output - y
+    d = [out - y]
+    i = len(theta) - 1
+    while len(d) < len(theta):
+        # d value that is currently first is the last layer we 'backpropagated'
+        d_ = d[0]
+        t_ = theta[i]
+        i -= 1
 
-    z2 = z[0]
-    z3 = z[1]
+        # theta[i] corresponds to z[i-1] - there is no theta[L] and there is no z[0]
+        z_ = z[i]
 
-    d3 = a3 - y
-    d2 = np.multiply((d3 * theta2[:, 1:]), g_prime(z2))
+        # compute the d value for this layer
+        new_value = np.multiply((d_ * t_[:, 1:]), g_prime(z_))
+        d.insert(0, new_value)
 
-    delta1 = d2.T*a1
-    delta2 = d3.T*a2
+    big_delta = []
+    theta_p = []
+    # compute big delta value & theta_prime values
+    for (ix, d_) in enumerate(d):
+        # the 'first' d value is d2; multiply it by the first a value a1 - and so on
+        big_delta.append(d_.T * a[ix])
+        theta_p.append(big_delta[-1] / m)
 
-    theta1_prime = delta1 / m
-    theta2_prime = delta2 / m
+    return theta_p
 
-    return [theta1_prime, theta2_prime]

simple_machine.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lib.network import build_synapses, forward_propagate, delta, cost, a
+from lib.network import build_synapses, forward_propagate, delta, cost, a, theta_prime
 
 
 class SimpleMachine(object):
@@ -47,13 +47,32 @@ class SimpleMachine(object):
         np.random.seed(1)
 
         for i in range(iterations):
+            (a_0, z_0) = forward_propagate(self.training_data, self.theta)
+            print 'cost', cost(a_0[-1], self.Y)
+
             test = i % self.m
             x = self.training_data[test]
             y = self.Y[test]
 
-            (g, z_) = forward_propagate(x, self.theta)
-            # backward propagate deltas
+            (a_, z_) = forward_propagate(x, self.theta)
 
+            gradients = theta_prime(a_, z_, self.theta, y)
+
+            for (ix, g) in enumerate(gradients):
+                self.theta[ix] -= self.l * g.T
+
+        (a_final, z_final) = forward_propagate(self.training_data, self.theta)
+        print 'cost', cost(a_final[-1], self.Y)
+
+
+if __name__ == "__main__":
+    t = np.matrix(((1.0, 0, 0, 0.99), (0, 0.8, 0, 0.95), (0, 0, 0.9, 0.9)))
+    l = [4, 3]
+    r = np.matrix(((1, 0, 0), (0, 1, 0), (0, 0, 1)))
+    s = SimpleMachine(t, r, l)
+    s.train(100)
+
+'''
             # we need an error matrix for each layer
             errors = [None] * len(g)
 
@@ -72,13 +91,4 @@ class SimpleMachine(object):
             d0 = 1.0 / self.m * big_delta
             d = [np.matrix(1.0 / self.m * (big_delta + (self.l * th))) for th in self.theta]
             self.theta = [th + d[ix] for ix, th in enumerate(self.theta)]
-            print 'big delta', big_delta
-            print 'cost', cost(self.score(self.training_data), self.Y)
-
-
-if __name__ == "__main__":
-    t = np.matrix(((1.0, 0, 0, 0.99), (0, 0.8, 0, 0.95), (0, 0, 0.9, 0.9)))
-    l = [4, 3]
-    r = np.matrix(((1, 0, 0), (0, 1, 0), (0, 0, 1)))
-    s = SimpleMachine(t, r, l)
-    s.train(100)
+'''

test/forward_propagate_test.py
@@ -12,18 +12,19 @@ def test_single_synapse():
     (a, z) = forward_propagate(input_data, synapses)
 
     assert len(a) == 2
-    assert (a[0] == input_data).all()
+    assert (a[0][:, 1:] == input_data).all()
+    assert a[0][:, 1:].shape == input_data.shape
+
     shape = a[1].shape
+    assert shape == (4, 5)
 
-    assert shape == (4, 4)
-
-    print a[1]
     # values are all between 0.997527 and 0.997528
-    assert (np.zeros(shape) < (a[1] - 0.9990889)).all()
-    assert (np.zeros(shape) > (a[1] - 0.9990890)).all()
+    assert (np.zeros(shape) < (a[1] - 0.9990889))[:, 1:].all()
+    assert (np.zeros(shape) > (a[1] - 0.9990890))[:, 1:].all()
 
     assert len(z) == 1
-    assert z[0].shape == a[1].shape
+    assert z[0].shape[0] == a[1].shape[0]
+    assert z[0].shape[1] + 1 == a[1].shape[1]
 
 
 def test_three_synapse():
@@ -36,25 +37,35 @@ def test_three_synapse():
     (a, z) = forward_propagate(input_data, synapses)
 
     assert len(a) == 4
-    assert (a[0] == input_data).all()
+    assert (a[0][:, 1:] == input_data).all()
 
     shape = a[1].shape
-    assert shape == (4, 50)
-    assert (np.zeros(shape) < (a[1] - 0.99999999)).all()
-    assert (np.zeros(shape) > (a[1] - 1.00000001)).all()
+    assert shape == (4, 51)
+    assert (np.zeros(shape) < (a[1] - 0.99999999))[:, 1:].all()
+    assert (np.zeros(shape) > (a[1] - 1.00000001))[:, 1:].all()
 
     print a[2]
     shape = a[2].shape
-    assert shape == (4, 20)
-    assert (np.zeros(shape) < (a[2] - 0.99999999)).all()
-    assert (np.zeros(shape) > (a[2] - 1.00000001)).all()
+    assert shape == (4, 21)
+    assert (np.zeros(shape) < (a[2] - 0.99999999))[:, 1:].all()
+    assert (np.zeros(shape) > (a[2] - 1.00000001))[:, 1:].all()
 
     shape = a[3].shape
-    assert shape == (4, 5)
-    assert (np.zeros(shape) < (a[3] - 0.99999999)).all()
-    assert (np.zeros(shape) > (a[3] - 1.00000001)).all()
+    assert shape == (4, 6)
+    assert (np.zeros(shape) < (a[3] - 0.99999999))[:, 1:].all()
+    assert (np.zeros(shape) > (a[3] - 1.00000001))[:, 1:].all()
 
     assert len(z) == 3
-    assert z[0].shape == a[1].shape
-    assert z[1].shape == a[2].shape
-    assert z[2].shape == a[3].shape
+    assert z[0].shape[0] == a[1].shape[0]
+    assert z[0].shape[1] + 1 == a[1].shape[1]
+
+    assert z[1].shape[0] == a[2].shape[0]
+    assert z[1].shape[1] + 1 == a[2].shape[1]
+
+    assert z[2].shape[0] == a[3].shape[0]
+    assert z[2].shape[1] + 1 == a[3].shape[1]
+
+
+if __name__ == "__main__":
+    test_single_synapse()
+    test_three_synapse()